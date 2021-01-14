1841eefedafac0761ac6d5ed12335d916dab4e17
Hhyeonho <hh3999@ks.ac.kr>
2021-01-14 22:18:27 +0900

    'comm'

--- terrarium/script.js
+++ terrarium/script.js
@@ -3,34 +3,31 @@ dragElement(document.getElementById('plant2'));
 dragElement(document.getElementById('plant3'));
 dragElement(document.getElementById('plant4'));
 
-/*"A closure is the combination of a function bundled together (enclosed) 
-with references to its surrounding state (the lexical environment). 
-In other words, a closure gives you access to an outer function’s scope 
-from an inner function." Create a closure so that you can track the dragged element*/
-
 function dragElement(terrariumElement) {
     //set 4 positions for positioning on the screen
     let pos1 = 0,
         pos2 = 0,
         pos3 = 0,
         pos4 = 0;
-    terrariumElement.onpointerdown = pointerDrag;
+    terrariumElement.onpointerdown = pointerDrag; // 마우스 누를 때
 
     function pointerDrag(e) {
         e.preventDefault();
         console.log(e);
-        // get the initial mouse cursor position for pos3 and pos4
+        // clientX : 브라우저 페이지에서의 X좌표 위치를 반환
+        // clienty : 브라우저 페이지에서의 y좌표 위치를 반환
         pos3 = e.clientX;
         pos4 = e.clientY;
-        // when the mouse moves, start the drag
+        // when the mouse moves, start the drag > 마우스 움직이며, 드래그 할 때
         document.onpointermove = elementDrag;
-        // when the mouse is lifted, stop the drag
+        // when the mouse is lifted, stop the drag > 마우스 떼고, 드래그 멈출 때
         document.onpointerup = stopElementDrag;
+        const para = document.querySelector('p');
     }
 
     function elementDrag(e) {
         // calculate the new cursor position
-        // pos1 = where the Xmouse WAS - where it IS
+        // pos1 = where the Xmouse WAS - where it ISIS
         pos1 = pos3 - e.clientX;
         // pos2 = where the Ymouse WAS - where it IS
         pos2 = pos4 - e.clientY;
